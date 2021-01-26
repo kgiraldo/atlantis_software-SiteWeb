c7afb130f5520494e3a4277d30d1bdfa57f05934
Karol Giraldo <kgiraldoatlantis@DESKTOP-Q7NUST5.localdomain>
2021-01-26 14:40:01 +0100

    Fix: menu mobile view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener} from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild} from '@angular/core';
 import { Router } from '@angular/router';
 import {MatDialog } from '@angular/material/dialog';
 import {ContactModalComponent} from './contact-modal/contact-modal.component';
@@ -11,11 +11,13 @@ import {ContactModalComponent} from './contact-modal/contact-modal.component';
 export class AppComponent {
   title = 'atlantis-web';
 
-  showMenu?:boolean;
-  isHome?:boolean;
-
+  showMenu:boolean;
+  isHome:boolean;
+  colorLogo:string;
+  @ViewChild('sc_content_menu', {static:true}) menu: ElementRef;
 
   constructor(private router:Router, public dialog: MatDialog){
+
     //Init show Menu variable
     if(window.innerWidth <=800){
       this.showMenu=false;
@@ -28,10 +30,17 @@ export class AppComponent {
     }else{
       this.isHome =false;
     }
-    console.log(this.isHome)
   }
 
   ngOnInit(): void {
+     //Color
+     if(window.innerWidth <=800 && this.isHome===true  ){
+      this.colorLogo = 'white_logo';
+    }else if(window.innerWidth <=800 && this.isHome===false ){
+      this.colorLogo = 'blue_logo';
+    } else{
+      this.colorLogo = 'blue_logo';
+    }
   }
 
   ngDoCheck(){
@@ -40,39 +49,23 @@ export class AppComponent {
     }else{
       this.isHome =false;
     }
+
+    //Color
+    if(window.innerWidth <=800 && this.isHome  ){
+      this.colorLogo = 'white_logo';
+    }else if(window.innerWidth <=800 && !this.isHome ){
+      this.colorLogo = 'blue_logo';
+    } else{
+      this.colorLogo = 'blue_logo';
+    }
   }
 
   
   openDialog(): void {
     const dialogRef = this.dialog.open(ContactModalComponent, {
       width: 'fit-content',
-      data: {},
       panelClass: 'custom-modalbox'
     });
-  }
-
-  //Visualisation Menu Conditions
-  getMenuStyleWhite(){
-    if(this.isHome && window.innerWidth <=800){
-      return  'flex';
-    } else{
-       return 'none';
-    } 
-  }
-
-  getMenuStyleBlue(){
-    if(this.isHome && window.innerWidth <=800){
-      return 'none';
-    } else{
-      return 'flex';
-    }
-  }
-  getBackgroundColorMenu(){
-    if(this.isHome && window.innerWidth <=800){
-      return '#133854';
-    } else{
-      return 'none';
-    }
   }
 
   //Get menu option Information
@@ -102,11 +95,19 @@ export class AppComponent {
   }
 
   openMenuMobile(){ 
-    this.showMenu=true;
+    (this.menu.nativeElement as HTMLElement).style.right = "0";
+    (this.menu.nativeElement as HTMLElement).style.opacity = "1";
+
   }
 
   closeMenuMobile(){
-    this.showMenu=false;
+    (this.menu.nativeElement as HTMLElement).style.right = "-100%";
+    (this.menu.nativeElement as HTMLElement).style.opacity = "0";
+  }
+
+  onClickMenu(url:string){
+    this.router.navigate([url]);
+    this.closeMenuMobile();
   }
 
   
